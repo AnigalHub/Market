19574aa6ceeea153152c3d80fb972eef030ea69c
AnigalHub <anigal1997@gmail.com>
2021-07-30 23:08:40 +0300

    компонент на выбор сортировки

---
+++ components/select.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+      <div id="order">
+        <div id="sort">Сортировать по:</div>
+        <div id="price">цене</div>
+        <svg id="arrow" width="5" height="3" viewBox="0 0 5 3" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5 0H0L2.5 2.5L5 0Z" fill="#59606D"/>
+        </svg>
+        <div id="option">
+          <div>По цене</div>
+          <div>По популярности</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "select"
+    }
+</script>
+
+<style scoped>
+  #sort{
+    height: 21px;
+    top:42px;
+    left: 1191px;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 21px;
+    display: flex;
+    align-items: flex-end;
+  }
+  #price{
+    width: 34px;
+    height: 21px;
+    left: 1309px;
+    top: 42px;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 21px;
+    display: flex;
+    align-items: flex-end;
+    color: #59606D;
+  }
+</style>
