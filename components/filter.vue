acfa43ed072476d9d740ad2c5f651ccae0b9eb44
AnigalHub <anigal1997@gmail.com>
2021-07-31 18:31:23 +0300

    добавлен компонент на фильтрацию каталога

---
+++ components/filter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="option">
+    <div>По цене</div>
+    <div>По популярности</div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "filter"
+    }
+</script>
+
+<style scoped>
+
+</style>
