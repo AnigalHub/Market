943fb6ca5925d7154003a4326f8b35f8c084282c
AnigalHub <anigal1997@gmail.com>
2021-08-24 23:59:32 +0300

    добавлены методы на открытие и закрытие коризны

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,54 @@
 <template>
   <div>
-    <navbar/>
+    <navbar v-on:basketClicked="showBasket"/>
     <div class="background">
       <catalog/>
+      <basket v-if="isBasketShowing" v-on:closeClicked="hideBasket"/>
     </div>
   </div>
 </template>
 
 <script>
   import Catalog from "~/components/catalog";
+  import Basket from "~/components/basket";
   export default {
-    components: {Catalog}
+    components: {Basket, Catalog},
+    data(){
+      return{
+        isBasketShowing:false,
+      }
+    },
+    methods:{
+      showBasket: function () {
+        this.isBasketShowing = true;
+      },
+      hideBasket:function () {
+        this.isBasketShowing = false;
+      }
+    }
   }
 </script>
 
-<style>
+<style  lang="scss">
   body{
-    margin:  0 0 !important;
+    margin: 0 !important;
+  }
+  h1{
+    margin: 0 !important;
   }
   .background{
     margin-left: auto;
     margin-right: auto;
     width: 1440px;
+    position: relative;
   }
+  .flex-container{display: flex;}
 
+  nav{
+    ul{margin-left: -40px !important;}
+    li{
+      display: flex;
+      align-items: flex-end;
+    }
+  }
 </style>
