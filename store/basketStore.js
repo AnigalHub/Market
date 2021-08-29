333fd2c163197053f6421d137820ca724aa868a6
AnigalHub <anigal1997@gmail.com>
2021-08-29 22:17:42 +0300

    добавлено сохранение состояния (actions,setSavedBasket (localStorage))

--- store/basketStore.js
+++ store/basketStore.js
@@ -1,19 +1,40 @@
 export const state = () =>({
-  products:[]
+  products: []
 })
 export const mutations ={
   addProduct(state, product){
-    state.products.push(product)
+    state.products.push(product);
   },
   deleteProduct(state,id){
     state.products.splice(id,1)
   },
   cleanBasket(state){
     state.products=[]
+  },
+  setSavedBasket(state, savedBasket){
+    state.products = savedBasket
   }
 }
-export const getters ={
+export const getters = {
   Products(state){
-    return state.products
+     return state.products
+  }
+}
+export const actions = {
+  refreshSavedState(context){
+    if (!process.browser)
+      return;
+    let newProducts = JSON.parse(localStorage.getItem('basket'));
+    console.log(newProducts)
+    if(newProducts)
+      context.commit('setSavedBasket',newProducts)
+  },
+  addProductState(context,product){
+    context.commit('addProduct',product)
+    localStorage.setItem('basket', JSON.stringify(context.getters.Products))
+  },
+  deleteProductState(context, id) {
+    context.commit('deleteProduct',id)
+    localStorage.setItem('basket', JSON.stringify(context.getters.Products))
   }
 }
