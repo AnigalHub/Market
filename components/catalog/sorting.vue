61d260c7af37686902dd6b265926ab2332846ae3
AnigalHub <anigal1997@gmail.com>
2021-10-07 05:12:23 +0300

    добавлен dropdown (не работает)

--- components/catalog/sorting.vue
+++ components/catalog/sorting.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="sorting">
-    <div id="sort">Сортировать по:</div>
-    <div id="name_sort">цене</div>
-    <component :is="arrow"/>
-    <div id="filter">
-      <nav>
-        <ul>
-          <li v-for="name in Filter"><a>{{name.name}}</a></li>
-        </ul>
-      </nav>
-    </div>
+      <div class="dropdown">
+      Сортировать по: {{sort}}  </div>
+      <component :is="arrow"/>
+      <div class="dropdown-content">
+        <div @click="SortByPrice()">По цене</div>
+        <div @click="SortByNamePopular()">По популярности</div>
+      </div>
+
   </div>
 </template>
 
@@ -19,9 +17,18 @@
       name: "sorting",
       data() {
         return {
+          sort:"цене",
           arrow: Arrow_filterSVG,
           Filter:[{name:"По цене"},{name:"По популярности"}],
         }
+      },
+      methods:{
+        SortByPrice:function () {
+          this.sort = "цене"
+        },
+        SortByNamePopular:function () {
+          this.sort = "поп.."
+        },
       }
     }
 </script>
@@ -32,34 +39,45 @@
    padding: 0 0 0 992px;
    float: left;
  }
- #name_sort{
-   width: 34px;
-   padding: 0 0 0 6px;
-   color: #59606D;
- }
- #sort,#name_sort{
+ .dropdown {
+   float: left;
    height: 21px;
-   padding-top:42px;
+   border: none;
+   margin-left: 992px;
+   padding: 42px 5px 11px 0;
    font-size: 16px;
    line-height: 21px;
-   display: flex;
-   align-items: flex-end;
+   position: relative;
+   display: inline-block;
+   cursor: pointer;
    font-family: 'PT Sans', sans-serif;
  }
- #filter{
-   width: 160px;
-   height: 68px;
+ .dropdown-content div {
+   font-family: 'PT Sans', sans-serif;
+   border-radius: 4px;
+   background: #FFFEFB;
+ }
+ .dropdown-content {
+   //display: none;
    position: absolute;
-   right: 88px;
-   top: 69px;
-   padding: 8px 0 12px;
    background: #FFFFFF;
    box-shadow: 0 4px 16px rgba(0, 0, 0, 0.05);
    border-radius: 8px;
-   li{
+   width: 160px;
+   height: 68px;
+   padding: 8px 0 12px;
+   top: 69px;
+   left: 1192px;
+   cursor: pointer;
+   z-index: 1;
+   div{
      height: 26px;
+     color:#959DAD;
+     font-size: 16px;
+     line-height: 21px;
      padding: 4px 0 4px 12px;
      &:hover{
+       color: #1F1F1F;
        background: #F8F8F8;
        cursor: pointer;
      }
