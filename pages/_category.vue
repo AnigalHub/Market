a863599dbe273a5415d1bc8dc40e55e4f0290324
AnigalHub <anigal1997@gmail.com>
2021-10-19 14:40:02 +0300

    добавлено в fetch - условия на разные адреса (группировка)

---
+++ pages/_category.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <navbar v-on:basketClicked="showBasket"/>
+    <div class="background">
+      <catalog :Products="Products"/>
+      <basket v-if="isBasketShowing" v-on:closeClicked="hideBasket"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Catalog from "~/components/catalog";
+  import Basket from "~/components/basket";
+  export default {
+    components: {Basket, Catalog},
+    data(){
+      return{
+        isBasketShowing:false,
+        Products: [],
+      }
+    },
+    async fetch(){
+      console.log(this.$route.params.category)
+      try{
+        if(this.$route.params.category == 'Рюкзаки'){
+          this.Products = (await this.$axios.get('https://frontend-test.idalite.com/api/product?category=1')).data
+        }
+        else if(this.$route.params.category == 'Портфели'){
+          this.Products = (await this.$axios.get('https://frontend-test.idalite.com/api/product?category=3')).data
+        }
+        else if(this.$route.params.category == 'Сумки'){
+          this.Products = (await this.$axios.get('https://frontend-test.idalite.com/api/product?category=2')).data
+        }
+        else {
+          this.Products = (await this.$axios.get('https://frontend-test.idalite.com/api/product')).data
+        }
+      } catch (error) {
+        console.log("Ошибка:", error)
+      }
+    },
+    methods:{
+      showBasket: function () {
+        this.isBasketShowing = true;
+      },
+      hideBasket:function () {
+        this.isBasketShowing = false;
+      },
+
+    },
+    created: function () {
+      this.$store.dispatch('basketStore/refreshSavedState')
+    }
+  }
+</script>
+
+<style  lang="scss">
+  body{margin: 0 !important;}
+  h1{margin: 0 !important;}
+  .background{
+    margin-left: auto;
+    margin-right: auto;
+    width: 1440px;
+    position: relative;
+  }
+  .flex-container{display: flex;}
+
+  nav{
+    ul{margin-left: -40px !important;}
+    li{
+      display: flex;
+      align-items: flex-end;
+    }
+
+
+  }
+</style>
+<style>
+  @media screen and (min-width: 1220px) and (max-width: 1460px) {
+    .background{width: 1200px !important;}
+  }
+  @media screen and (min-width: 1020px) and (max-width: 1220px) {
+    .background{width: 992px !important;}
+  }
+  @media screen and (min-width: 860px) and (max-width: 1020px) {
+    .background{width: 835px !important;}
+  }
+  @media screen and (min-width: 670px) and (max-width: 860px) {
+    .background{width: 650px !important;}
+  }
+  @media screen and (min-width: 530px) and (max-width: 670px) {
+    .background{width: 500px !important;}
+  }
+  @media screen and  (max-width: 530px) {
+    .background{width: 100% !important;}
+  }
+</style>
